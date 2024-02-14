6a1b732369349381011246f0a02da23a7b7ad232
Sharad Bisi <sharadbisi778@gmail.com>
2024-02-14 17:38:15 +0530

    adding video in tools screen

--- lib/main_screens/tools.dart
+++ lib/main_screens/tools.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:url_launcher/url_launcher.dart';
 import 'tool.dart';
 
 class ToolsScreen extends StatefulWidget {
@@ -10,38 +11,40 @@ class ToolsScreen extends StatefulWidget {
 }
 
 class _ToolsScreenState extends State<ToolsScreen> {
-
   //defining list of tools
   final List<Tool> tools = [
-  Tool(
-    name: "Tractor",
-    description: "Useful for plowing, tilling, and planting large fields.",
-    imageUrl: "images/tractor.jpg",
-    videoUrl: "https://www.youtube.com/shorts/mZnSH4pY6PQ",
-  ),
-  Tool(
-    name: "Hand Plow",
-    description: "Ideal for small gardens and soft soils.",
-    imageUrl: "images/handplow.jpeg",
-    videoUrl: "https://www.youtube.com/shorts/mZnSH4pY6PQ",
-  ),
-  // Add more tools as needed
-];
-
+    Tool(
+      name: "Tractor",
+      description: "Useful for plowing, tilling, and planting large fields.",
+      imageUrl: "images/tractor.jpg",
+      videoUrl: "https://www.youtube.com/shorts/mZnSH4pY6PQ",
+    ),
+    Tool(
+      name: "Hand Plow",
+      description: "Ideal for small gardens and soft soils.",
+      imageUrl: "images/handplow.jpeg",
+      videoUrl: "https://www.youtube.com/shorts/mZnSH4pY6PQ",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(centerTitle: true,
-      title: Text('Tools'),
-      backgroundColor: Colors.blueGrey[300],
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Tools'),
+        backgroundColor: Colors.blueGrey[300],
       ),
-           body: ListView.builder(
+      body: ListView.builder(
         itemCount: tools.length,
         itemBuilder: (context, index) {
           final tool = tools[index];
           return ListTile(
-            leading: Image.asset(tool.imageUrl, width: 50, height: 50), // Make sure to add actual image assets
+            leading: Image.asset(
+              tool.imageUrl,
+              width: 50,
+              height: 50,
+            ),
             title: Text(tool.name),
             subtitle: Text(tool.description),
             onTap: () {
@@ -78,15 +81,25 @@ class ToolDetailPage extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Text(tool.description),
             ),
-            // This is a placeholder for the video or a link to it
-            // In a real app, you might use a package like `flutter_youtube` or `webview_flutter` to embed the video
             TextButton(
-              onPressed: () {/* Open video URL */tool.videoUrl;},
-              child: Text("Watch Demo Video"),
+              onPressed: () {
+                // tool.videoUrl;
+                _openUrl(tool.videoUrl);
+              },
+              child: const Text("Watch Demo Video"),
             ),
           ],
         ),
       ),
     );
   }
+
+  Future<void> _openUrl(String videoUrl) async {
+    final Uri url = Uri.parse(videoUrl);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+
 }
