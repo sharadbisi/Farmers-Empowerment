3ae3a08089e275f4220587a7c71ed8bda1c2e0be
Sharad Bisi <sharadbisi778@gmail.com>
2024-02-22 23:23:06 +0530

    weather api is initialized

--- lib/navbar_screens/weather/weather.dart
+++ lib/navbar_screens/weather/weather.dart
@@ -1,3 +1,5 @@
+import 'package:farmer/navbar_screens/weather/weather_model.dart';
+import 'package:farmer/navbar_screens/weather/weather_services.dart';
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -8,6 +10,37 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  //api key
+  final _weatherService = WeatherService('0837bb789f0ea1efe2e8b12ecbb992d0');
+  Weather? _weather;
+
+  //fetch weather
+  _fetchweather() async {
+    //get the city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for that city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    //is any error
+    catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+
+  //initial state
+  @override
+  void initState() {
+    super.initState();
+    _fetchweather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +50,19 @@ class _WeatherScreenState extends State<WeatherScreen> {
         shadowColor: Colors.white,
         backgroundColor: Colors.orange[300],
       ),
-      backgroundColor: Colors.lightGreen,
-      body: Center(child: Text('WEATHER')),
+      // backgroundColor: Colors.lightGreen,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //city name
+            Text(_weather?.cityName ?? "loding city...."),
+
+            //tempreture
+            Text('${_weather?.temp.round()}°C')
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/navbar_screens/weather/weather_services.dart
@@ -0,0 +1,44 @@
+import 'dart:convert'; // This is necessary for jsonDecode
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'weather_model.dart';
+
+class WeatherService {
+  // It's better to name constants with a full uppercase to follow Dart conventions
+  static const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity()  async{
+    //get permission from user
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+    }
+
+    //fetch the current location
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high
+    );
+
+    //convert the location into a list of placemark object
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    //exctract the city name from the first placemark
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
+  }
+}
